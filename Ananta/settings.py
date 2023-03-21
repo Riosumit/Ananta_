34b5a6cee0958bf2b151f39dc4b319c02db0eca9
Modern-artist <90668597+Modern-artist@users.noreply.github.com>
2023-03-21 07:05:08 +0530

    deployment

--- Ananta/settings.py
+++ Ananta/settings.py
@@ -134,3 +134,6 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if not DEBUG:
+    CSRF_TRUSTED_ORIGINS = ["https://web-production-f94a.up.railway.app/"]
